3bdc6a6167f0491776331cbce98364a97906254d
sysstas <sysstas@gmail.com>
2018-03-27 17:39:52 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+var express = require('express')
+var app = express()
+
+app.set('port', (process.env.PORT || 3000))
+
+app.use(express.static('dist'))
+
+app.listen(app.get('port'), function(){
+    console.log('Server is listen on ', app.get('port'))
+})
