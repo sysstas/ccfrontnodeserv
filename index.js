a205b0328d347e3037f2763423a2a9ade65190b5
sysstas <sysstas@gmail.com>
2018-04-13 23:54:52 +0300

    cannot get bug resolve

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 var express = require('express')
 var app = express()
-
+var path = require('path');
 app.set('port', (process.env.PORT || 3000))
 
 app.use(express.static('app'))
+app.route('/*').get(function(req, res) { 
+    return res.sendFile(path.join( __dirname+'/app/index.html')); 
+});
 
 app.listen(app.get('port'), function(){
     console.log('Server is listen on ', app.get('port'))
